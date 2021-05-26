eab7b4547e2783906296e3f8a2a13cb6637a811d
Justice Towett <jtowett89@gmail.com>
2021-05-26 16:00:13 +0300

    fonts change

--- src/App.js
+++ src/App.js
@@ -16,90 +16,88 @@ class App extends Component {
   }
   render() {
     return (
-      <>
-        <Router>
-          <Nav />
-          <Switch>
-            <Route
-              exact
-              path="/sample-react-website/"
-              component={(props) => {
-                return (
-                  <Home
-                    {...props}
-                    text={`Welcome to XYZ Company`}
-                    tagline={`We are here to serve you!`}
-                    bgUrl={`https://images.wallpaperscraft.com/image/night_city_street_car_113387_1280x720.jpg`}
-                    bannerHeight={`100vh`}
-                    showTagline={true}
-                  />
-                );
-              }}
-            />
-            <Route
-              path="/sample-react-website/about/"
-              component={(props) => {
-                return (
-                  <About
-                    {...props}
-                    text={`About Us`}
-                    tagline={``}
-                    bgUrl={`https://images.wallpaperscraft.com/image/night_city_street_road_158135_1280x720.jpg`}
-                    bannerHeight={`50vh`}
-                    showTagline={false}
-                  />
-                );
-              }}
-            />
-            <Route
-              path="/sample-react-website/services/"
-              component={(props) => {
-                return (
-                  <Services
-                    {...props}
-                    text="Our Services"
-                    tagline={``}
-                    bgUrl={`https://images.wallpaperscraft.com/image/night_city_road_buildings_186588_1280x720.jpg`}
-                    bannerHeight={`50vh`}
-                    showTagline={false}
-                  />
-                );
-              }}
-            />
-            <Route
-              path="/sample-react-website/gallery/"
-              component={(props) => {
-                return (
-                  <Gallery
-                    {...props}
-                    text="Gallery"
-                    tagline={``}
-                    bgUrl={`https://images.wallpaperscraft.com/image/night_city_buildings_road_140907_1280x720.jpg`}
-                    bannerHeight={`50vh`}
-                    showTagline={false}
-                  />
-                );
-              }}
-            />
-            <Route
-              path="/sample-react-website/contact/"
-              component={(props) => {
-                return (
-                  <Contact
-                    {...props}
-                    text={`Contact Us`}
-                    tagline={``}
-                    bgUrl={`https://images.wallpaperscraft.com/image/night_city_skyscrapers_buildings_181631_1280x720.jpg`}
-                    bannerHeight={`50vh`}
-                    showTagline={false}
-                  />
-                );
-              }}
-            />
-          </Switch>
-          <Footer />
-        </Router>
-      </>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route
+            exact
+            path="/sample-react-website/"
+            component={(props) => {
+              return (
+                <Home
+                  {...props}
+                  text={`Welcome to XYZ Company`}
+                  tagline={`We are here to serve you!`}
+                  bgUrl={`https://images.wallpaperscraft.com/image/night_city_street_car_113387_1280x720.jpg`}
+                  bannerHeight={`100vh`}
+                  showTagline={true}
+                />
+              );
+            }}
+          />
+          <Route
+            path="/sample-react-website/about/"
+            component={(props) => {
+              return (
+                <About
+                  {...props}
+                  text={`About Us`}
+                  tagline={``}
+                  bgUrl={`https://images.wallpaperscraft.com/image/night_city_street_road_158135_1280x720.jpg`}
+                  bannerHeight={`50vh`}
+                  showTagline={false}
+                />
+              );
+            }}
+          />
+          <Route
+            path="/sample-react-website/services/"
+            component={(props) => {
+              return (
+                <Services
+                  {...props}
+                  text="Our Services"
+                  tagline={``}
+                  bgUrl={`https://images.wallpaperscraft.com/image/night_city_road_buildings_186588_1280x720.jpg`}
+                  bannerHeight={`50vh`}
+                  showTagline={false}
+                />
+              );
+            }}
+          />
+          <Route
+            path="/sample-react-website/gallery/"
+            component={(props) => {
+              return (
+                <Gallery
+                  {...props}
+                  text="Gallery"
+                  tagline={``}
+                  bgUrl={`https://images.wallpaperscraft.com/image/night_city_buildings_road_140907_1280x720.jpg`}
+                  bannerHeight={`50vh`}
+                  showTagline={false}
+                />
+              );
+            }}
+          />
+          <Route
+            path="/sample-react-website/contact/"
+            component={(props) => {
+              return (
+                <Contact
+                  {...props}
+                  text={`Contact Us`}
+                  tagline={``}
+                  bgUrl={`https://images.wallpaperscraft.com/image/night_city_skyscrapers_buildings_181631_1280x720.jpg`}
+                  bannerHeight={`50vh`}
+                  showTagline={false}
+                />
+              );
+            }}
+          />
+        </Switch>
+        <Footer />
+      </Router>
     );
   }
 }
